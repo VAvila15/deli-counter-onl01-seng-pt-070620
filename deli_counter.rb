e475e273bfff8c61e033af91d3fe087d0b28a02d
Victor Avila <avilavictor1570@gmail.com>
2020-07-17 02:18:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,10 @@
 def line (katz_deli)
   if katz_deli.length == 0
   puts "The line is currently empty."
+  else
+    current_line = "The line is curently:"
+    deli.each.with_index(1) do |person, Integer|
+      current_line << #{Integer}. #{person}
  end
+ puts current_line
 end
